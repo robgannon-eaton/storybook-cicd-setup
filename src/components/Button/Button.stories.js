e112e2facf12cb30cc8452dddf33aacc16c84765
robjganno <robertjgannon@eaton.com>
2022-03-09 09:14:28 +0000

    test

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,7 +7,8 @@ export default {
 }
 
 
-export const Primary = () => <Button variant = 'primary'>Primary</Button>
+export const Primary = () => <Button variant = 'primary'>CHANGED</Button>
 export const Secondary = () => <Button variant = 'secondary'>Primary</Button>
 export const Success = () => <Button variant = 'success'>Primary</Button>
 export const Danger = () => <Button variant = 'danger'>Primary</Button>
+
